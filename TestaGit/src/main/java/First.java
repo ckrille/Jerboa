d6d7ff212df901a46f7d7bf137c0f07de04257d2
Christoffer Deurell <christoffer.deurell@live.se>
2021-03-20 19:21:56 +0100

    second

---
+++ TestaGit/src/main/java/First.java
@@ -0,0 +1,17 @@
+public class First {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+
+        System.out.print();
+
+        doThis();
+
+    }
+
+    private static void doThis(){
+        System.out.println();
+    }
+
+}
